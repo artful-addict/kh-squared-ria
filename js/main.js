8fcfa5531efff5690e892b95c2543720b8eababb
Artful Addict <akinskevin86@gmail.com>
2017-04-16 14:53:56 -0700

    Updated Landing Page

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-// Change background of .navbar on scroll
+// Change background of .navigation on scroll
 $(function() {
 
 	$(window).on("scroll", function() {
 
 		if($(window).scrollTop() > 53) {
 
-			$('.navbar').css('background-color', 'rgba(0, 0, 0, .55)');
+			$('.navigation').css('background-color', 'rgba(0, 0, 0, .55)');
 		} else {
 
 			// remove the background property so it becomes transparent again
-			$('.navbar').css('background-color', 'transparent');
+			$('.navigation').css('background-color', 'transparent');
 		}
 	});
 });
