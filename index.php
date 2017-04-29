758de853bb4dc0e7c4626df90c4d6b2a0d605703
Kevin Akins <info@kevinakins.com>
2017-04-29 16:33:58 -0700

    Updating Contact Form

--- index.php
+++ index.php
@@ -1,20 +1,68 @@
 <?php
-    // Page Title
-    $pageTitle = "KH-Squared RIA";
-    // Page Author
-    $pageAuthor = "Kevin Akins";
-    // Page Description
+    if (empty($_post) === false) {
+        $errors = array();
 
-    // Page Keywords
+        $firstName      = $_POST['firstName'];
+        $lastName       = $_POST['lastName'];
+        $phoneNumber    = $_POST['phoneNumber'];
+        $emailAddress   = $_POST['emailAddress'];
+        $subject        = $_POST['subject'];
+        $message        = $_POST['message'];
 
-    // Styles Path
-    $stylesPath = "css/styles.css";
-
+        if (empty($firstName) === true || empty($emailAddress) === true || empty($message) === true) {
+            $errors[] = 'Name, email, and message are required!';
+        } else {
+            if (filter_var($emailAddress, FILTER_VALIDATE_EMAIL) === false) {
+                $erros[] = 'That\'s not a valid email address';
+            }
+            if (ctype_alpha($firstName) === false) {
+                $errors[] = 'Name must only contain letters!';
+            }
+        }
+    }
 ?>
 
-<?php include('inc/layout/header.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>KH-Squared RIA</title>
 
-        <!-- Full-Menu toggles off and on in mobile -->
+        <link href="css/styles.css" rel="stylesheet">
+        <script src="https://use.fontawesome.com/dc56619f8a.js"></script>
+
+        <link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="img/favicons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="img/favicons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="img/favicons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="img/favicons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="img/favicons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="img/favicons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="img/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="img/favicons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="img/favicons/favicon-16x16.png">
+        <link rel="manifest" href="img/favicons/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="img/favicons/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,700" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
         <div class="full-menu"> 
 
             <div class="nav-bar">
@@ -66,10 +114,8 @@
             </div> <!-- /.container -->
         </div> <!-- /.full-menu -->
 
-        <!-- Navigation houses the nav-bar and desktop-nav-->
         <div class="navigation">
 
-            <!-- Nav-Bar for mobile devices -->
             <div class="nav-bar">
 
                 <div class="container">
@@ -79,17 +125,15 @@
                     </div>
 
                     <div class="toggle-item">
-                        <a href="contact"><span class="fa fa-envelope"></span></a>
+                        <span class="fa fa-envelope"></span>
                     </div>
                 </div> <!-- /.container -->
             </div> <!-- /.navbar -->
 
-            <!-- Desktop-Nav for desktops and large monitors -->
             <div class="desktop-nav">
 
                 <div class="container">
 
-                    <!-- Brand-Group displays the company logo and any text-based titles -->
                     <div class="brand-group">
 
                         <img
@@ -100,7 +144,6 @@
                         <div class="hb"><h1>KH&ndash;Squared</h1></div>
                     </div> <!-- /.brand-group -->
 
-                    <!-- Desktop-Nav-Menu is atraditional, horizontal menu in the Desktop-Nav -->
                     <div class="desktop-nav-menu">
                         <ul class="nav-list">
                             <li><a href="#contact">Contact</a></li>
@@ -113,10 +156,8 @@
             </div> <!-- /.desktop-nav -->
         </div> <!-- /.navigation -->
 
-        <!-- Hero Section is an area of free realestate used to display important information above the fold -->
         <div id="hero">
 
-            <!-- The Brand-group utilized for mobile devices -->
             <div class="brand-group">
 
                 <img
@@ -127,13 +168,11 @@
                 <div class="hb">KH&ndash;Squared</div>
             </div> <!-- /.brand-group -->
 
-            <!-- Scrolls down to the next section of the web page -->
             <div class="call-to-action">
 
                 <span class="fa fa-angle-double-down"></span>
             </div>
 
-            <!-- A primary-yet-subsection of the Hero Section that displays a contact form above the fold -->
             <div id="contact">
                 <div class="container">
 
@@ -156,139 +195,91 @@
                             </div> <!-- /.contact-group -->
                         </div>
 
-                        <div class="col-sm-9">
+                        <div class="col-sm-9" id="desktop-form">
+                            <?php
+                                if (empty($errors) === false) {
+                                    echo '<ul>';
+                                    foreach($errors as $error) {
+                                        echo '<li>'
+                                    }
+                                    echo '</ul>';
+                                }
+                            ?>
+                            <form id="contactForm" action="success.php" method="post">
 
-                        		<?php
-    
-				                    // Check for header injections
-				                    function has_header_injection($str) {
-				                        return preg_match( "/[\r\n]/", $str );
-				                    }
+                                <div class="row">
 
-				                    if (isset ($_POST['contact_submit'])) {
-				                        $firstName 			= trim($_POST['firstName']);
-				                        $lastName 			= trim($_POST['lastName']);
-				                        $phoneNumber		= $_POST['phoneNumber'];
-				                        $emailAddress 	    = trim($_POST['emailAdress']);
-				                        $subject 	        = $_POST['subject'];
-				                        $msg 			    = $_POST['message'];
+                                    <div class="col-xs-12 col-sm-6">
 
-				                        // Check to see if $name or $email have header injections
-				                        if (has_header_injection($firstName) || has_header_injection($lastName) || has_header_injection($phoneNumber) || has_header_injection($email)) {
-				                            die(); // If true, kill the script
-				                        }
+                                        <div class="form-group">
 
-				                        if ( !$firstName || !$lastName || !$phoneNumber || !$email || !$msg ) {
-				                            echo '<div class="error-group"><h4 class="error">All fields required.</h4><a href="index.php" class="block">Go back and try again</a></div>';
-				                            exit;
-				                        }
+                                            <label class="sr-only" for="firstName">First Name</label>
+                                            <input class="form-control" type="text" name="firstName" id="firstName" placeholder="First Name">
+                                        </div>
+                                    </div>
 
-				                        // Add the recipient email to a variable
-				                        $to = "akinskevin86@gmail.com";
+                                    <div class="col-xs-12 col-sm-6">
 
-				                        // Create a subject
-				                        $subject = "Inquiree Message";
+                                        <div class="form-group">
 
-				                        // Construct the message
-				                        $message = "First Name: $firstName\r\n";
-				                        $message .= "Last Name: $lastName\r\n";
-				                        $message .= "Phone: $phoneNumber\n\r";
-				                        $message .= "Email: $emailAddress\r\n";
-				                        $message .= "Subject: $subject\r\n";
-				                        $message .= "Message: \r\n$msg";
+                                            <label class="sr-only" for="lastName">Last Name</label>
+                                            <input class="form-control" type="text" name="lastName" id="lastName" placeholder="Last Name">
+                                        </div>
+                                    </div>
+                                </div>
 
-				                        $message = wordwrap($message, 72);
+                                <div class="row">
 
-				                        // Set the mail headers into a variable
-				                        $headers = "MIME-Version: 1.0\r\n";
-				                        $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
-				                        $headers .= "From: $lastName, $firstName <$emailAddress> \r\n";
-				                        $headers .= "X-Priority: 1\r\n";
-				                        $headers .= "X-MSMail-Priority: High\r\n\r\n";
+                                    <div class="col-xs-12 col-sm-6">
 
-				                        // Send the email
-				                        mail($to, $subject, $message, $headers);
-				                ?>
+                                        <div class="form-group">
 
-				                <?php } else { ?>
+                                            <label class="sr-only" for="phoneNumber">Phone&#40;Optional&#41;</label>
+                                            <input class="form-control" type="tel" name="phoneNumber" id="phoneNumber" placeholder="Phone (Optional)">
+                                        </div>
+                                    </div>
 
-	                            <form id="contact-form" action="success.php" method="post">
+                                    <div class="col-xs-12 col-sm-6">
 
-	                                <div class="row">
+                                        <div class="form-group">
 
-	                                    <div class="col-xs-12 col-sm-6">
+                                            <label class="sr-only" for="emailAddress">E&ndash;mail</label>
+                                            <input class="form-control" type="email" name="emailAddress" id="emailAddress" placeholder="E-mail">
+                                        </div>
+                                    </div>
+                                </div>
 
-	                                        <div class="form-group">
+                                <div class="row">
 
-	                                            <label class="sr-only" for="firstName">First Name</label>
-	                                            <input class="form-control" type="text" name="firstName" id="firstName" placeholder="First Name">
-	                                        </div>
-	                                    </div>
+                                    <div class="col-xs-12 col-sm-12">
 
-	                                    <div class="col-xs-12 col-sm-6">
+                                        <div class="form-group">
 
-	                                        <div class="form-group">
+                                            <label class="sr-only" for="subject">Subject</label>
+                                            <input class="form-control" type="text" name="subject" id="subject" placeholder="Subject">
+                                        </div>
+                                    </div>
+                                </div>
 
-	                                            <label class="sr-only" for="lastName">Last Name</label>
-	                                            <input class="form-control" type="text" name="lastName" id="lastName" placeholder="Last Name">
-	                                        </div>
-	                                    </div>
-	                                </div>
+                                <div class="row">
 
-	                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12">
 
-	                                    <div class="col-xs-12 col-sm-6">
+                                        <textarea class="form-control" id="message" name="message" placeholder="Your Message Here..." row="1"></textarea>
+                                    </div>
 
-	                                        <div class="form-group">
+                                    <div class="col-xs-12">
 
-	                                            <label class="sr-only" for="phoneNumber">Phone&#40;Optional&#41;</label>
-	                                            <input class="form-control" type="tel" name="phoneNumber" id="phoneNumber" placeholder="Phone (Optional)">
-	                                        </div>
-	                                    </div>
-
-	                                    <div class="col-xs-12 col-sm-6">
-
-	                                        <div class="form-group">
-
-	                                            <label class="sr-only" for="emailAddress">E&ndash;mail</label>
-	                                            <input class="form-control" type="email" name="emailAddress" id="emailAddress" placeholder="E-mail">
-	                                        </div>
-	                                    </div>
-	                                </div>
-
-	                                <div class="row">
-
-	                                    <div class="col-xs-12 col-sm-12">
-
-	                                        <div class="form-group">
-
-	                                            <label class="sr-only" for="subject">Subject</label>
-	                                            <input class="form-control" type="text" name="subject" id="subject" placeholder="Subject">
-	                                        </div>
-	                                    </div>
-	                                </div>
-
-	                                <div class="row">
-
-	                                    <div class="col-xs-12 col-sm-12">
-
-	                                        <textarea class="form-control" id="message" name="message" placeholder="Your Message Here..." row="1"></textarea>
-	                                    </div>
-
-	                                    <div class="col-xs-12">
-
-	                                        <input class="btn-kh btn-long" type="submit" value="Send">
-	                                    </div>
-	                                </div>
-	                            </form>
-		                        <?php } ?>
-                        </div> <!-- /.col-sm-9 -->
+                                        <input class="btn-kh btn-long" type="submit" value="Send">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div> <!-- /.row -->
                 </div> <!-- /.container -->
             </div> <!-- /#contact -->
         </div> <!-- /#hero -->
 
-        <!-- The tray is a fluid-width entity that holds the other main sections of the page; seperating itself from the hero -->
         <div class="tray">
 
             <div id="overview">
@@ -381,4 +372,69 @@
             </div> <!-- /#about -->
         </div> <!-- /.tray -->
 
-<?php include('inc/layout/footer.php'); ?>
+        <div class="footer">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-sm-12">
+
+                        <p style="text-align: center;">KH&ndash;Squared RIA. &copy; 2017. All Rights Reserved.</p>
+                    </div> <!-- /.twelve columns -->
+                </div> <!-- /.row -->
+            </div> <!-- /.container -->
+        </div> <!-- /.footer -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/app.min.js"></script>
+    <script>
+        
+        $(document).ready(function () {
+
+            $('.toggle-nav').on('click', function () {
+                
+                $('.full-menu').toggleClass('full-menu--open');
+            });
+
+            $('.full-menu').on('click', function() {
+                
+                $('.full-menu').removeClass('full-menu--open');
+            })
+
+            $('.toggle-item').on('click', function() {
+                
+                $('.full-form').toggleClass('full-form--open');
+            });
+
+            $(".call-to-action").click(function() {
+                
+                $('html,body').animate({
+                    
+                    scrollTop: $("#overview").offset().top},
+                    'slow');
+            });
+
+            $(".call-to-top").click(function() {
+                
+                $('html,body').animate({
+                    
+                    scrollTop: $("#hero").offset().top},
+                    'slow');
+            });
+        });
+
+        $(window).resize(function() {
+            var $form = $('#contactForm');
+            $('#mobile-form').append($form);
+            if ($('html,body').width() <= 767) {
+                $('#mobile-form').append($form);
+            } else {
+                if ($('html,body').width() >= 768) {
+                    $('#desktop-form').append($form);
+                }
+            }
+        });
+    </script>
+  </body>
+</html>
